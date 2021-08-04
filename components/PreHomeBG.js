b5c4434a913f4aad6e324cf593669de6d4b95ac9
brightxu <brightxu@umich.edu>
2021-08-04 02:44:00 -0400

    made image sizes dynamic to window size

--- components/PreHomeBG.js
+++ components/PreHomeBG.js
@@ -1,38 +1,37 @@
 import { useEffect, useState } from 'react';
-import { getWD } from './WindowDimensions.js'
 import styles from '../styles/ImageFadeIn.module.css'
 
 const PreHomeBG = ( { tracks, artists } ) => {
-  const positions = [{left: '35%', top: '40%'},
+  const positions = [{left: '34%', top: '41%'}, // pic ID = VSCode row # + 5
                      {left: '88%', top: '23%'},
-                     {left: '14%', top: '80%'},
-                     {left: '68%', top: '65%'},
+                     {left: '11%', top: '85%'},
+                     {left: '69%', top: '65%'},
                      {left: '55%', top: '50%'},
                      {left: '8%', top: '24%'},
                      {left: '61%', top: '17%'},
-                     {left: '86%', top: '84%'},
-                     {left: '43%', top: '75%'},
+                     {left: '86%', top: '86%'},
+                     {left: '43%', top: '73%'},
                      {left: '20%', top: '48%'},
-                     {left: '74%', top: '38%'},
-                     {left: '92%', top: '52%'},
+                     {left: '76%', top: '38%'},
+                     {left: '92%', top: '50%'},
                      {left: '30%', top: '70%'},
                      {left: '27%', top: '18%'},
-                     {left: '57%', top: '87%'},
+                     {left: '56%', top: '87%'},
                      {left: '49%', top: '28%'},
                      {left: '8%', top: '56%'},
                      {left: '82%', top: '57%'},
-                     {left: '74%', top: '20%'},
+                     {left: '73%', top: '19%'},
                      {left: '31%', top: '91%'},
-                     {left: '40%', top: '13%'},
+                     {left: '40%', top: '15%'},
                      {left: '74%', top: '86%'},
                      {left: '45%', top: '51%'},
                      {left: '18%', top: '25%'},
                      {left: '56%', top: '69%'},
                      {left: '65%', top: '39%'},
-                     {left: '10%', top: '10%'},
-                     {left: '10%', top: '10%'},
-                     {left: '10%', top: '10%'},
-                     {left: '10%', top: '10%'}];
+                     {left: '19%', top: '66%'},
+                     {left: '44%', top: '92%'},
+                     {left: '65%', top: '86%'},
+                     {left: '92%', top: '67%'}];
 
 
   const [count, setCount] = useState(0);
@@ -83,33 +82,9 @@ const PreHomeBG = ( { tracks, artists } ) => {
   }, []);
 
 
-
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
-
-  function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
-  }
-
-
   if (tracks.length !== 0 && artists.length !== 0) {
-    //const { height, width } = useWindowDimensions();
+    const baseSize = (window.innerHeight + window.innerWidth) / 13; // 13 and 615 are just numbers I picked
+    const changingSize = (window.innerHeight + window.innerWidth) / 615; 
 
     if (!gotSrcs) {
       setSrcs(getPicSrcs());
@@ -119,18 +94,12 @@ const PreHomeBG = ( { tracks, artists } ) => {
     return(
       <div>
         {srcs.slice(0, count).map((src, id) => (
-          <div key={id} style={{position: 'fixed', left: `${positions[id].left}`, top: `${positions[id].top}`, transform: 'translate(-50%, -50%)', zIndex: `${(id + 1) * -1}`}}>
-            <img src={src} height={150 - (id * 3.5)} className={styles.fadeInImg}/>
+          <div key={id} style={{position: 'fixed', left: `${positions[id].left}`, top: `${positions[id].top}`, 
+                                transform: 'translate(-50%, -50%)', zIndex: `${(id + 1) * -1}`}}>
+            <img src={src} height={baseSize - (id * changingSize)} width={baseSize - (id * changingSize)} className={styles.fadeInImg}/>
             {id}
           </div>
         ))}
-        <br /><br /><br /><br /><br /><br /><br />
-        <div>
-          height: {window.innerHeight}
-        </div>
-        <div>
-          width: {window.innerWidth}
-        </div>
       </div>
     );
   }
