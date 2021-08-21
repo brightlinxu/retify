c50a1b9a3771403cce9a5612ecf990abb18022f4
brightxu <brightxu@umich.edu>
2021-08-21 01:39:19 -0400

    continued working on index page

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 const Index = () => {
   const windowSize = getWindowSize();
   const rememberStr = 'REMEMBER';
+  const rediscoverStr = 'REDISCOVER';
 
 
   return (
@@ -16,7 +17,8 @@ const Index = () => {
           <img src='/images/rewind.png' className={styles.section1Img}/>
         </Parallax>
       </div>
-      <div className={styles.section2}>
+      <div className={styles.section2} style={{height: `${windowSize.width > windowSize.height ? 100 : 50}vh`}}>
+        <img src='images/scribble4.png' className={styles.section2Img} />
         <span>
           {rememberStr.split('').map((letter, id) => (
             <Parallax key={id} x={[`0px`, `${windowSize.width ? windowSize.width * ((id / 25) - (3 / 23)) : 0}px`]}
@@ -27,18 +29,38 @@ const Index = () => {
             </Parallax>
           ))}
         </span>
+        <img src='images/scribble5.png' className={styles.section2Img} />
       </div>
-      <div className={styles.section3}>
-        <Parallax y={[80, -80]}>
-          <div className={styles.section3Img}>
+      <div className={styles.section3} style={{height: `${windowSize.width > windowSize.height ? 130 : 65}vh`}}>
+        <Parallax y={[`${windowSize.width > 600 ? -200 : -100}px`, '100px']} className={styles.section3BG}>
+          <img src='/images/scribble8.png' style={{height: `${windowSize.width / 1.7}px`, width: '70vw'}}/>
+        </Parallax>
+        <div className={styles.section3Group1}>
+          <Parallax y={[`${windowSize.width > 600 ? -80 : -60}px`, '60px']} className={styles.section3Img}>
             <img src='/images/remember.png' style={{width: '100%', height: '100%', borderRadius: '10px'}}/>
-          </div>
-        </Parallax>
-        <Parallax y={[-100, 100]}>
-          <div style={{fontSize: `${(windowSize.width / 100) + 10}px`}} className={styles.section3Box}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          </div>
-        </Parallax>
+          </Parallax>
+          <Parallax y={[0, 0]}>
+            <div style={{fontSize: `${(windowSize.width / 90) + 8}px`}} className={styles.section3Box}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            </div>
+          </Parallax>
+        </div>
+      </div>
+      <div className={styles.section4} style={{height: `${windowSize.width > windowSize.height ? 60 : 30}vh`}}>
+        <span>
+          {rediscoverStr.split('').map((letter, id) => (
+            <Parallax key={id} y={[`${((4.5 - id) ** 2) * -5}`, `${((4.5 - id) ** 2) * 5}`]}
+            styleInner={{fontSize: `${windowSize.width / 18}px`, fontFamily: 'Montserrat-SemiBold', 
+                         letterSpacing: `${(windowSize.width / 60) + 5}px`}}
+            className={styles.section4Text}
+            >
+              {letter}
+            </Parallax>
+          ))}
+        </span>
+      </div>
+      <div className={styles.section5}>
+        filler
       </div>
 
 
