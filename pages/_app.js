fae1e6861c19e85cc5dbf8d353fe71d80fac0e34
Bright Xu <brightxu@umich.edu>
2021-09-10 01:34:28 -0400

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Layout from '../components/Layout'
 import { ParallaxProvider } from 'react-scroll-parallax';
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
       <Layout>
+        <Head>
+          <title>retify</title>
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ParallaxProvider>
